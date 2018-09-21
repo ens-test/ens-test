36f4bba8a03b2320f0f6f13d5460632be65a97e7
Kevin Chen <kevin.zchn@gmail.com>
2018-09-21 19:53:32 -0400

    Enable fifs, disable auction.

--- scripts/30_deploy_ens.sh
+++ scripts/30_deploy_ens.sh
@@ -5,5 +5,5 @@ SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 rm subs/ens/build/contracts/ENSRegistry.json
 cd subs/ens
 npm i
-# truffle migrate --network env.fifs --reset
-truffle migrate --network env.auction --reset
+truffle migrate --network env.fifs --reset
+# truffle migrate --network env.auction --reset
